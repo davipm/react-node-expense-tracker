b113e67db793ef38f76dca57353cf3e49364c2ff
Davi Pereira <davi.p.m94@gmail.com>
2025-11-06 10:41:42 +0900

    update

--- packages/api/src/context.ts
+++ packages/api/src/context.ts
@@ -1,8 +1,8 @@
 export async function createContext(opts: any) {
-	// No auth configured
-	return {
-		session: null,
-	};
+  console.log(opts);
+  return {
+    session: null,
+  };
 }
 
 export type Context = Awaited<ReturnType<typeof createContext>>;
